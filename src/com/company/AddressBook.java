749578fac5010cf21666f18a31cf549a53ccb07a
Lauren Dennedy <laurendennedy@gmail.com>
2020-02-11 07:32:25 -0800

    Added code to AddressBook init method

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -2,6 +2,9 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 // AddressBook class
 public class AddressBook {
@@ -23,6 +26,38 @@ public class AddressBook {
 
     // init function to start AddressBook with its own file
     public static void init(String filename) {
+        try {
+            // Set up objects to begin reading the file
+            File file = new File(filename);
+            Scanner fileReader = new Scanner(file);
+            String fName, lName, street, city, state, tel, email;
+            int zip;
+            int index = 0;
 
+            // Read all the contents in the file (while new line)
+            while (fileReader.hasNextLine()) {
+                switch (index) {
+                    case 0: fName = fileReader.nextLine();
+                    case 1: lName = fileReader.nextLine();
+                    case 2: street = fileReader.nextLine();
+                    case 3: city = fileReader.nextLine();
+                    case 4: state = fileReader.nextLine();
+                    case 5:
+                        String temp = fileReader.nextLine();
+                        try {
+                            zip = Integer.parseInt(temp);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Improper zip in file.");
+                        }
+                    case 6: email = fileReader.nextLine();
+                    case 7: tel = fileReader.nextLine();
+                }
+                index = (index + 1) % 8;
+            }
+
+        // Catch exception if at any point file could not be used
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be found.");
+        }
     }
 }
